a62b577577e1f17063dc6651dbca7798cb8df00f
kusun0307-code <kusun0307@gmail.com>
2026-07-23 15:04:09 +0900

    Deploy school production API bbbffeb

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.EXAM_MANAGEMENT_CONFIG = Object.freeze({
-  API_URL: "__APPS_SCRIPT_WEB_APP_URL__",
+  API_URL: "https://script.google.com/macros/s/AKfycbz--kzc-9cQDIbyysWRBQo1vEkJVdvs2fn68TvMSx1_JdS_XvRBNANoXBQmxpYDKYob/exec",
   APP_NAME: "한양대학교사범대학부속고등학교 통합 시스템",
 });
